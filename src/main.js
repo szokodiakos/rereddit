248b0b2a24573a9d3fa2e080c7c44154ee0fec5f
Akos Szokodi <szokodiakos@gmail.com>
2017-10-05 20:48:36 +0200

    added analyics

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import Vue from 'vue';
 import VueTippy from 'vue-tippy';
 import VueResource from 'vue-resource';
+import VueAnalytics from 'vue-analytics';
 import Buefy from 'buefy';
 import moment from 'moment';
 import 'buefy/lib/buefy.min.css';
@@ -30,6 +31,13 @@ moment.updateLocale('en', {
 Vue.use(Buefy);
 Vue.use(VueResource);
 Vue.use(VueTippy);
+Vue.use(VueAnalytics, {
+  id: 'UA-100520893-2',
+  router,
+  autoTracking: {
+    exception: true,
+  },
+});
 
 /* eslint-disable no-new */
 new Vue({
